26561c627507b3398bdd0b4808d2d70ec8e33462
yashshah7197 <yash@yashshah.io>
2023-11-27 07:47:10 -0500

    Update comments

--- iter/iterable.go
+++ iter/iterable.go
@@ -2,5 +2,6 @@ package iter
 
 // Iterable is an interface for collections that can be iterated over.
 type Iterable[T any] interface {
+	// Iterator returns an iterator over elements of type T.
 	Iterator() Iterator[T]
 }
--- iter/iterator.go
+++ iter/iterator.go
@@ -2,7 +2,10 @@ package iter
 
 // Iterator is an interface for iterating over a collection.
 type Iterator[T any] interface {
+	// HasNext returns true if the iteration has more elements.
 	HasNext() bool
+	// Next returns the next element in the iteration.
 	Next() T
+	// Remove removes from the underlying collection the last element returned by this iterator.
 	Remove()
 }
